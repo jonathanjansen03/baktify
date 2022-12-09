1032b6ce35fcd8239a82dc5cd680e360ff09e668
Jonathan Jansen Subroto <jonathan.jansen03@gmail.com>
2022-12-09 18:16:49 +0700

    feat: added Home Page

---
+++ resources/views/includes/guest-navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="#">
+    <img src="{{ asset('images/logo.png') }}" alt="logo" id="logo">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div id="mainNavbar" class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto mx-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="">About us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">Products</a>
+      </li>
+    </ul>
+    <form class="form-inline">
+      <button id="sign_in_btn" class="btn">Sign in</button>
+      <button id="sign_up_btn" class="btn">Sign up</button>
+    </form>
+  </div>
+</nav>
