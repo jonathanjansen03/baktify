29e93cd258e551a141ac0fed5122a19db997b9bc
Jonathan Jansen Subroto <jonathan.jansen03@gmail.com>
2022-12-11 16:32:04 +0700

    feat: added About Us Page

---
+++ resources/views/pages/about-us.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app')
+
+@section('title', 'About Us')
+@section('main-content')
+    <div id="about_us">
+      <p class="text-center">
+        "Baktify started from our love for music and album, we created Baktify to spread the love in out music community. We started off from a street near Rawa Belong street and now we have the most diverse collection of albums and musics. We won't stop until music spreads all over the world!"
+      </p>
+      <h6 class="text-center text-black">Conny Blue, CEO Baktify</h6>
+    </div>
+
+    <hr>
+
+    <div class="about-us-table">
+      <div class="row">
+        <div class="col"><h5 class="text-black">Get in touch</h5></div>
+        <div class="col">
+          <h6 class="text-black">Sales Inquiry</h6>
+          <p>
+            sales@baktify.com<br>
+            +62 1231 1231
+          </p>
+        </div>
+        <div class="col">
+          <h6 class="text-black">Sales Inquiry</h6>
+          <p>
+            sales@baktify.com<br>
+            +62 1231 1231
+          </p>
+        </div>
+      </div>
+
+      <div class="row justify-content-end">
+        <div class="col-4">
+          <h6 class="text-black">Sales Inquiry</h6>
+          <p>
+            sales@baktify.com<br>
+            +62 1231 1231
+          </p>
+        </div>
+        <div class="col-4">
+          <h6 class="text-black">Sales Inquiry</h6>
+          <p>
+            sales@baktify.com<br>
+            +62 1231 1231
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="about-us-table">
+      <div class="row">
+        <div class="col"><h5 class="text-black">Location</h5></div>
+        <div class="col">
+          <h6 class="text-black">Jakarta</h6>
+          <p>
+            Jl. Rawa Belong No. 420<br>
+            11420
+          </p>
+        </div>
+        <div class="col">
+          <h6 class="text-black">Jakarta</h6>
+          <p>
+            Jl. Rawa Belong No. 420<br>
+            11420
+          </p>
+        </div>
+      </div>
+
+      <div class="row justify-content-end">
+        <div class="col-4">
+          <h6 class="text-black">Jakarta</h6>
+          <p>
+            Jl. Rawa Belong No. 420<br>
+            11420
+          </p>
+        </div>
+        <div class="col-4">
+          <h6 class="text-black">Jakarta</h6>
+          <p>
+            Jl. Rawa Belong No. 420<br>
+            11420
+          </p>
+        </div>
+      </div>
+    </div>
+@endsection
