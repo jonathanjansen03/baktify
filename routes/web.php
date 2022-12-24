9437ca5d9934846b20d3bdbdaa31cf38c379d688
ConvexHull89 <riosheren89@gmail.com>
2022-12-24 15:30:15 +0700

    add register controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('pages.home');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/about-us', function () {
     return view('pages.about-us');
 });
+
+Route::get('sign-up', [RegisterController::class, 'registerPage'])->name('signup');
