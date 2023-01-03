82ae62e01fb87a93c2462e695e638e591cb2b8f9
Jonathan Jansen Subroto <jonathan.jansen03@gmail.com>
2023-01-04 00:59:13 +0700

    style: merged sign in and sign up styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,7 @@
     <link rel="stylesheet" href="{{ asset('/styles/app.css') }}">
     <link rel="stylesheet" href="{{ asset('/styles/home.css') }}">
     <link rel="stylesheet" href="{{ asset('/styles/about-us.css') }}">
-    <link rel="stylesheet" href="{{ asset('/styles/sign-up.css') }}">
-    <link rel="stylesheet" href="{{ asset('/styles/sign-in.css') }}">
+    <link rel="stylesheet" href="{{ asset('/styles/register-sign-in.css') }}">
     <link rel="stylesheet" href="{{ asset('/styles/product-detail.css') }}">
     <link rel="stylesheet" href="{{ asset('/styles/product-list.css') }}">
     <link rel="stylesheet" href="{{ asset('/styles/insert-product.css') }}">
